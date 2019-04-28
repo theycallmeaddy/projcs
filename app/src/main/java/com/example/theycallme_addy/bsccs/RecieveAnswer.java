bdfde4d30b9eb304f671dae5426acb143fffa462
theycallmeaddy <adimishra4298@gmail.com>
2019-04-28 15:33:44 +0530

    Assignment Submit Option

---
+++ app/src/main/java/com/example/theycallme_addy/bsccs/RecieveAnswer.java
@@ -0,0 +1,22 @@
+package com.example.theycallme_addy.bsccs;
+
+public class RecieveAnswer {
+    public String answer;
+
+    public RecieveAnswer(){
+
+    }
+
+    public RecieveAnswer(String answer){
+        this.answer = answer;
+
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
